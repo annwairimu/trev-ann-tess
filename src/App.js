3119b499e48bc0d5376adf7542d50ef699fefdc6
Trevor2002-maker <trevor.madara@student.moringaschool.com>
2023-06-20 22:47:12 +0300

    Done with Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Route,Routes} from 'react-router-dom';
-
+import {Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Project from './Pages/Project/Project'
+import Contact from './Pages/Contact/Contact'
 
 function App() {
   return(
-    <>
-    </>
+    <div>
+      <Routes>
+        <Route path = '/' element={<Home />} />
+        <Route path = '/about' element={<About />} />
+        <Route path = '/project' element={<Project />} />
+        <Route path = '/contact' element={<Contact />} />
+      </Routes>
+    </div>
   )
 }
 
